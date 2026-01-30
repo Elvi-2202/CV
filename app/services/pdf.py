5863fc77fc76dca1719abb74fdfb12aa88dc15c3
Elvi-2202 <156439254+Elvi-2202@users.noreply.github.com>
2026-01-30 18:16:05 +0100

    ajout des documentations utilisées dans le readme

--- app/services/pdf.py
+++ app/services/pdf.py
@@ -1,10 +1,10 @@
-import pdfplumber
-import pdf
+# import pdfplumber
+# import pdf
 
-def extract_text(pdf_path):
-    with pdfplumber.open(pdf_path) :
-        text = ""
-        for page in pdf.pages:
-            page_text = page.extract_text()
-            text += page_text + "\n"
-        return text.strip()
+# def extract_text(pdf_path):
+#     with pdfplumber.open(pdf_path) :
+#         text = ""
+#         for page in pdf.pages:
+#             page_text = page.extract_text()
+#             text += page_text + "\n"
+#         return text.strip()
